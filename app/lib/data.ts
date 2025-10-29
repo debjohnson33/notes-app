c30a16aae1db37b1e49d8b5dd2d3ef869a9769a4
Deb Johnson <1.deborah.r.johnson@gmail.com>
2025-10-29 15:45:59 -0500

    Add placeholder data

---
+++ app/lib/data.ts
@@ -0,0 +1,25 @@
+const notes = [
+  {
+    id: 1,
+    title: "Note 1",
+    content: "This is the first note",
+  },
+  {
+    id: 2,
+    title: "Note 2",
+    content: "This is the second note",
+  },
+  {
+    id: 3,
+    title: "Note 3",
+    content: "This is the third note",
+  },
+  {
+    id: 4,
+    title: "Note 4",
+    content: "This is the fourth note",
+  },
+];
+export default notes;
+
+// TO DO: Add username or id? to each note
