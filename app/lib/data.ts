a0f794ccad0a8ead6447fed1a2ceb9143f1f82a8
Deb Johnson <1.deborah.r.johnson@gmail.com>
2025-10-29 16:27:29 -0500

    Add user data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,25 +1,41 @@
-const notes = [
+export const notes = [
   {
     id: 1,
+    userId: 1,
     title: "Note 1",
     content: "This is the first note",
   },
   {
     id: 2,
+    userId: 1,
     title: "Note 2",
     content: "This is the second note",
   },
   {
     id: 3,
+    userId: 2,
     title: "Note 3",
     content: "This is the third note",
   },
   {
     id: 4,
+    userId: 1,
     title: "Note 4",
     content: "This is the fourth note",
   },
 ];
-export default notes;
 
-// TO DO: Add username or id? to each note
+export const users = [
+  {
+    id: 1,
+    username: "John Doe",
+    email: "john.doe@example.com",
+    password: "password123",
+  },
+  {
+    id: 2,
+    username: "Jane Doe",
+    email: "jane.doe@example.com",
+    password: "password456",
+  },
+];
