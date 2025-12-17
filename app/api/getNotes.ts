fcafadc50728ae5efec873e82f45ed6a376b6c75
Deb Johnson <1.deborah.r.johnson@gmail.com>
2025-12-17 11:44:36 -0600

    Edit getNotes function

--- app/api/getNotes.ts
+++ app/api/getNotes.ts
@@ -2,11 +2,15 @@ import db from "@/app/lib/database";
 
 const getNotes = async (req, res) => {
   try {
-    const collection = db.collection("notes");
+    // Get the user from the request body
+    const { userId } = req.body;
 
-    const records = await collection.find({}).toArray();
-
-    res.status(200).json(records);
+    // Create a new note in the database
+    const currentUser = await db.collection("users").findOne({ id: userId });
+    if (currentUser) {
+      const records = await currentUser.notes;
+      res.status(200).json(records);
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error retrieving records" });
